0dfcdd37cad0495301b104099c37f11662a84163
DawnDIY <chenglu1990@gmail.com>
2015-07-28 23:53:35 +0800

    完善搜索

--- movie-scope.go
+++ movie-scope.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"strconv"
 	"strings"
-	//"net/http"
 )
 
 const homeMovieTemplate = `{
@@ -23,7 +22,8 @@ const homeMovieTemplate = `{
 			"field": "art",
 			"aspect-ratio": 0.6
 		},
-		"subtitle": "subtitle"
+		"subtitle": "subtitle",
+        "attributes": "attributes"
 	}
 }`
 
@@ -32,8 +32,7 @@ const hotMovieTemplate = `{
 	"template": {
 		"category-layout": "grid",
 		"card-layout": "vertical",
-		"card-size": "medium",
-		"overlay": true
+		"card-size": "medium"
 	},
 	"components": {
 		"title": "title",
@@ -41,7 +40,8 @@ const hotMovieTemplate = `{
 			"field": "art",
 			"aspect-ratio": 0.8
 		},
-		"subtitle": "subtitle"
+		"subtitle": "subtitle",
+        "attributes": "attributes"
 	}
 }`
 
@@ -59,7 +59,26 @@ const nearbyCinemaTemplate = `{
 			"field": "art",
 			"aspect-ratio": 1
 		},
-		"subtitle": "subtitle"
+		"subtitle": "subtitle",
+        "attributes": "attributes"
+	}
+}`
+
+const searchMovieTemplate = `{
+	"schema-version": 1,
+	"template": {
+        "category-layout": "grid",
+        "card-layout": "horizontal",
+        "card-size": "small"
+	},
+	"components": {
+		"title": "title",
+		"art": {
+			"field": "art",
+			"aspect-ratio": 1
+		},
+		"subtitle": "subtitle",
+        "attributes": "attributes"
 	}
 }`
 
@@ -97,9 +116,12 @@ func (m *MovieScope) Search(query *scopes.CannedQuery, metadata *scopes.SearchMe
 		log.Println("[搜索]: ", queryStr)
 		switch dptId {
 		case "":
-			m.searchHome(metadata, reply, queryStr)
+			m.searchMovie(metadata, reply, queryStr)
+			m.searchCinema(metadata, reply, queryStr)
 		case "hot_movie":
+			m.searchMovie(metadata, reply, queryStr)
 		case "nearby_cinema":
+			m.searchCinema(metadata, reply, queryStr)
 		}
 	}
 
@@ -117,6 +139,12 @@ func (m *MovieScope) Preview(result *scopes.Result, metadata *scopes.ActionMetad
 	} else if resType == "cinema" {
 		// 查看附近影院
 		m.viewNearbyCinema(result, reply)
+	} else if resType == "movie_search" {
+		// 搜索播放影片的影院的结果
+		m.viewSearchMovie(result, reply)
+	} else if resType == "cinema_search" {
+		// 搜索播放影片的影院的结果
+		m.viewSearchCinema(result, reply)
 	}
 	return nil
 }
@@ -161,10 +189,10 @@ func (m *MovieScope) showHome(metadata *scopes.SearchMetadata, reply *scopes.Sea
 		"location": loc,
 	}
 
-	json, err := GetAPIData(API_HOT_MOVIE, args)
+	jsonData, err := GetAPIData(API_HOT_MOVIE, args)
 	var movies []interface{}
 	if err == nil {
-		movies = json.GetPath("result", "movie").MustArray()
+		movies = jsonData.GetPath("result", "movie").MustArray()
 	}
 
 	category := reply.RegisterCategory("hot_movie", "热映影片", "", homeMovieTemplate)
@@ -181,6 +209,13 @@ func (m *MovieScope) showHome(metadata *scopes.SearchMetadata, reply *scopes.Sea
 		result.SetArt(fmt.Sprint(movie["movie_picture"]))
 		result.SetURI(fmt.Sprint(movie["movie_picture"]))
 		result.Set("subtitle", movie["movie_release_date"])
+
+		attrs := []map[string]interface{}{}
+		attrs = append(attrs, map[string]interface{}{
+			"icon":  "",
+			"value": "★ " + fmt.Sprint(movie["movie_score"]),
+		})
+		result.Set("attributes", attrs)
 
 		if err := reply.Push(result); err != nil {
 			log.Println(err)
@@ -202,10 +237,10 @@ func (m *MovieScope) showHotMovies(metadata *scopes.SearchMetadata, reply *scope
 		"location": loc,
 	}
 
-	json, err := GetAPIData(API_HOT_MOVIE, args)
+	jsonData, err := GetAPIData(API_HOT_MOVIE, args)
 	var movies []interface{}
 	if err == nil {
-		movies = json.GetPath("result", "movie").MustArray()
+		movies = jsonData.GetPath("result", "movie").MustArray()
 	}
 
 	category := reply.RegisterCategory("hot_movie", "热映影片", "", hotMovieTemplate)
@@ -222,6 +257,17 @@ func (m *MovieScope) showHotMovies(metadata *scopes.SearchMetadata, reply *scope
 		result.SetArt(fmt.Sprint(movie["movie_picture"]))
 		result.SetURI(fmt.Sprint(movie["movie_picture"]))
 		result.Set("subtitle", movie["movie_release_date"])
+
+		attrs := []map[string]interface{}{}
+		attrs = append(attrs, map[string]interface{}{
+			"icon":  "",
+			"value": "★ " + fmt.Sprint(movie["movie_score"]),
+		})
+		attrs = append(attrs, map[string]interface{}{
+			"icon":  "",
+			"value": fmt.Sprint(movie["movie_tags"]),
+		})
+		result.Set("attributes", attrs)
 
 		if err := reply.Push(result); err != nil {
 			log.Println(err)
@@ -244,10 +290,10 @@ func (m *MovieScope) showNearbyCinemas(metadata *scopes.SearchMetadata, reply *s
 		"location": loc,
 	}
 
-	json, err := GetAPIData(API_NEARBY_CINEMA, args)
+	jsonData, err := GetAPIData(API_NEARBY_CINEMA, args)
 	var cinemas []interface{}
 	if err == nil {
-		cinemas = json.GetPath("result").MustArray()
+		cinemas = jsonData.GetPath("result").MustArray()
 	}
 
 	category := reply.RegisterCategory("nearby_cinema", "周围影院", "", nearbyCinemaTemplate)
@@ -262,7 +308,14 @@ func (m *MovieScope) showNearbyCinemas(metadata *scopes.SearchMetadata, reply *s
 		result.SetTitle(fmt.Sprint(cinema["name"]))
 		result.SetArt(m.base.ScopeDirectory() + "/local.png")
 		result.SetURI(m.base.ScopeDirectory() + "/local.png")
-		result.Set("subtitle", cinema["rating"])
+		result.Set("subtitle", "距离 "+fmt.Sprint(cinema["distance"]))
+
+		attrs := []map[string]interface{}{}
+		attrs = append(attrs, map[string]interface{}{
+			"icon":  "",
+			"value": "★ " + fmt.Sprint(cinema["rating"]),
+		})
+		result.Set("attributes", attrs)
 
 		if err := reply.Push(result); err != nil {
 			log.Println(err)
@@ -297,7 +350,7 @@ func (m *MovieScope) viewHotMovie(result *scopes.Result, reply *scopes.PreviewRe
 	header.AddAttributeValue("title", "<b>"+fmt.Sprint(movie["movie_name"])+"</b>")
 
 	text := scopes.NewPreviewWidget("text", "text")
-	text.AddAttributeValue("title", "信息")
+	text.AddAttributeValue("title", "<b>信息</b>")
 	info := strings.Join([]string{
 		"评分: \t" + fmt.Sprint(movie["movie_score"]),
 		"类型: \t" + fmt.Sprint(movie["movie_type"]),
@@ -311,7 +364,7 @@ func (m *MovieScope) viewHotMovie(result *scopes.Result, reply *scopes.PreviewRe
 	text.AddAttributeValue("text", info)
 
 	description := scopes.NewPreviewWidget("description", "text")
-	description.AddAttributeValue("title", "描述")
+	description.AddAttributeValue("title", "<b>描述</b>")
 	description.AddAttributeValue("text", movie["movie_message"])
 
 	// ==========================
@@ -324,7 +377,7 @@ func (m *MovieScope) viewHotMovie(result *scopes.Result, reply *scopes.PreviewRe
 		"wd":       movieName,
 		"location": loc,
 	}
-	log.Println(param, loc)
+	// log.Println(param, loc)
 
 	jsonData, err := GetAPIData(API_SEARCH_MOVIE, param)
 	var cinemas []interface{}
@@ -334,7 +387,7 @@ func (m *MovieScope) viewHotMovie(result *scopes.Result, reply *scopes.PreviewRe
 
 	// 附近播放该影片的影院
 	nearbyCinemas := scopes.NewPreviewWidget("cinemas", "expandable")
-	nearbyCinemas.AddAttributeValue("title", "附近放映的影片")
+	nearbyCinemas.AddAttributeValue("title", "<b>附近放映的影院</b>")
 	nearbyCinemas.AddAttributeValue("collapsed-widgets", 2)
 
 	for i, cinema := range cinemas {
@@ -346,7 +399,6 @@ func (m *MovieScope) viewHotMovie(result *scopes.Result, reply *scopes.PreviewRe
 			"<br/>评分: " + fmt.Sprint(cinema["rating"]) +
 			"<br/>电话: " + fmt.Sprint(cinema["telephone"])
 		cInfo.AddAttributeValue("text", text)
-		log.Println(cinema["name"])
 
 		nearbyCinemas.AddWidget(cInfo)
 	}
@@ -366,7 +418,10 @@ func (m *MovieScope) viewNearbyCinema(result *scopes.Result, reply *scopes.Previ
 	var cinema map[string]interface{}
 	err := result.Get("map", &cinema)
 	// log.Println(cinema, err)
-	log.Println(err)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	layout1col := scopes.NewColumnLayout(1)
 	//layout2col := scopes.NewColumnLayout(2)
@@ -549,8 +604,430 @@ func (m *MovieScope) viewNearbyCinema(result *scopes.Result, reply *scopes.Previ
 	)
 }
 
-// 主页搜索
-func (m *MovieScope) searchHome(metadata *scopes.SearchMetadata, reply *scopes.SearchReply, queryStr string) {
+// 搜索播放影片的影院
+func (m *MovieScope) searchMovie(metadata *scopes.SearchMetadata, reply *scopes.SearchReply, queryStr string) {
+
+	location := metadata.Location()
+	loc := "116.3883,39.9289"
+	if location != nil {
+		loc = geoConv(location.Longitude, location.Latitude, false)
+	}
+
+	param := map[string]interface{}{
+		"wd":       queryStr,
+		"location": loc,
+	}
+
+	jsonData, err := GetAPIData(API_SEARCH_MOVIE, param)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	category := reply.RegisterCategory("search_movie", "影片影院搜索", "", searchMovieTemplate)
+
+	cinemas := jsonData.GetPath("result").MustArray()
+
+	for _, cinema := range cinemas {
+		cinema := cinema.(map[string]interface{})
+
+		result := scopes.NewCategorisedResult(category)
+
+		result.Set("type", "movie_search")
+		result.Set("loc", loc)
+		result.Set("map", cinema)
+		result.SetTitle(fmt.Sprint(cinema["name"]))
+		result.SetArt(m.base.ScopeDirectory() + "/local.png")
+		result.SetURI(m.base.ScopeDirectory() + "/local.png")
+		result.Set("subtitle", cinema["address"])
+
+		attrs := []map[string]interface{}{}
+		attrs = append(attrs, map[string]interface{}{
+			"icon":  "",
+			"value": "★ " + fmt.Sprint(cinema["rating"]),
+		})
+		result.Set("attributes", attrs)
+
+		if err := reply.Push(result); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+// 搜索播放影片的影院
+func (m *MovieScope) searchCinema(metadata *scopes.SearchMetadata, reply *scopes.SearchReply, queryStr string) {
+
+	location := metadata.Location()
+	loc := "116.3883,39.9289"
+	if location != nil {
+		loc = geoConv(location.Longitude, location.Latitude, false)
+	}
+
+	param := map[string]interface{}{
+		"wd":       queryStr,
+		"location": loc,
+	}
+
+	jsonData, err := GetAPIData(API_SEARCH_CINEMA, param)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	category := reply.RegisterCategory("search_cinema", "附近影院搜索", "", searchMovieTemplate)
+
+	cinemas := jsonData.GetPath("result").MustArray()
+
+	for _, cinema := range cinemas {
+		cinema := cinema.(map[string]interface{})
+
+		result := scopes.NewCategorisedResult(category)
+
+		result.Set("type", "cinema_search")
+		result.Set("loc", loc)
+		result.Set("map", cinema)
+		result.SetTitle(fmt.Sprint(cinema["name"]))
+		result.SetArt(m.base.ScopeDirectory() + "/local.png")
+		result.SetURI(m.base.ScopeDirectory() + "/local.png")
+		result.Set("subtitle", cinema["address"])
+
+		attrs := []map[string]interface{}{}
+		attrs = append(attrs, map[string]interface{}{
+			"icon":  "",
+			"value": "★ " + fmt.Sprint(cinema["rating"]),
+		})
+		result.Set("attributes", attrs)
+
+		if err := reply.Push(result); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (m *MovieScope) viewSearchMovie(result *scopes.Result, reply *scopes.PreviewReply) {
+
+	var cinema map[string]interface{}
+	err := result.Get("map", &cinema)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	layout1col := scopes.NewColumnLayout(1)
+	//layout2col := scopes.NewColumnLayout(2)
+	//layout3col := scopes.NewColumnLayout(3)
+
+	// Single column layout
+	layout1col.AddColumn(
+		"header",
+		"mapImage",
+		"info",
+		"actions",
+		"timeTable")
+
+	// Two column layout
+	//layout2col.AddColumn("image")
+	//layout2col.AddColumn("header", "summary", "actions")
+
+	// Three cokumn layout
+	//layout3col.AddColumn("image")
+	//layout3col.AddColumn("header", "summary", "actions")
+	//layout3col.AddColumn()
+
+	reply.RegisterLayout(layout1col)
+
+	// 标题头
+	header := scopes.NewPreviewWidget("header", "header")
+	header.AddAttributeValue("title", "<b>"+fmt.Sprint(cinema["name"])+"</b>")
+	header.AddAttributeValue("subtitle", cinema["address"])
+
+	// 地点坐标参数
+	lng := float64(0)
+	lat := float64(0)
+	location, ok := cinema["location"].(map[string]interface{})
+	if ok {
+		lng = location["lng"].(float64)
+		lat = location["lat"].(float64)
+	}
+	paramLoc := geoConv(lng, lat, true)
+	param := map[string]interface{}{
+		"center":       paramLoc,
+		"markers":      paramLoc,
+		"markerStyles": "l,o,0xff0000",
+		"zoom":         "17",
+	}
+	baiduLngLat := strings.Split(paramLoc, ",")
+	baiduMap := fmt.Sprintf("http://api.map.baidu.com/marker?location=%s,%s&title=影院地址&content=%s&output=html&src=Movie-Scope", baiduLngLat[1], baiduLngLat[0], cinema["name"])
+
+	// 静态地图
+	staticImgUrl := APIStaticImage(param)
+	param = map[string]interface{}{
+		"ak":       APP_KEY,
+		"location": paramLoc,
+		"width":    "1000",
+		"fov":      "270",
+	}
+	// 全景地图
+	panoramaImgUrl := APIPanoramaImage(param)
+
+	// 地图图片
+	mapImage := scopes.NewPreviewWidget("mapImage", "gallery")
+	mapImage.AddAttributeValue("fallback", m.base.ScopeDirectory()+"/local.png")
+	mapImage.AddAttributeValue("sources", []string{staticImgUrl, panoramaImgUrl})
+	mapImage.AddAttributeValue("zoomable", true)
+
+	info := scopes.NewPreviewWidget("info", "text")
+	info.AddAttributeValue("title", "<b>信息</b>")
+	info.AddAttributeValue(
+		"text",
+		"<b>电话: </b>"+fmt.Sprint(cinema["telephone"])+"<br/>"+
+			"<b>评分: </b>"+fmt.Sprint(cinema["rating"]))
+
+	// 按钮
+	actions := scopes.NewPreviewWidget("actions", "actions")
+	tuple1 := make(map[string]interface{})
+	tuple1["id"] = "tel"
+	tuple1["label"] = "电话咨询"
+	tuple1["uri"] = "tel:///" + fmt.Sprint(cinema["telephone"])
+	tuple2 := make(map[string]interface{})
+	tuple2["id"] = "map"
+	tuple2["label"] = "地图"
+	tuple2["uri"] = baiduMap
+	actions.AddAttributeValue("actions", []interface{}{tuple1, tuple2})
+
+	// 场次信息
+	timeTable := scopes.NewPreviewWidget("timeTable", "expandable")
+	timeTable.AddAttributeValue("collapsed-widgets", 2)
+	timeTable.AddAttributeValue("title", "<b>场次信息</b>")
+
+	tableData, ok := cinema["time_table"].([]interface{})
+	if ok {
+		for i, t := range tableData {
+			t := t.(map[string]interface{})
+			table := scopes.NewPreviewWidget("stable"+strconv.Itoa(i), "text")
+			table.AddAttributeValue("title", "")
+
+			time := fmt.Sprint(t["time"])
+			date := fmt.Sprint(t["date"])
+			lan := fmt.Sprint(t["lan"])
+			_type := fmt.Sprint(t["type"])
+			price := fmt.Sprint(t["price"])
+			if lan == "" && _type == "" {
+				lan = "暂无数据"
+			}
+			if price == "" {
+				price = "暂无数据"
+			}
+
+			text := "<b>时间:</b> " + date + " " + time +
+				"<br/><b>类型:</b> " + lan + _type +
+				"<br/><b>价格:</b> " + price
+			table.AddAttributeValue("text", text)
+
+			timeTable.AddWidget(table)
+		}
+	}
+
+	reply.PushWidgets(
+		mapImage,
+		header,
+		info,
+		actions,
+		timeTable,
+	)
+}
+
+func (m *MovieScope) viewSearchCinema(result *scopes.Result, reply *scopes.PreviewReply) {
+
+	var cinema map[string]interface{}
+	err := result.Get("map", &cinema)
+	// log.Println(cinema, err)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	layout1col := scopes.NewColumnLayout(1)
+	//layout2col := scopes.NewColumnLayout(2)
+	//layout3col := scopes.NewColumnLayout(3)
+
+	// Single column layout
+	layout1col.AddColumn(
+		"header",
+		"mapImage",
+		"info",
+		"actions",
+		"online",
+		"gallery",
+		"onMovies",
+		"reviews")
+
+	// Two column layout
+	//layout2col.AddColumn("image")
+	//layout2col.AddColumn("header", "summary", "actions")
+
+	// Three cokumn layout
+	//layout3col.AddColumn("image")
+	//layout3col.AddColumn("header", "summary", "actions")
+	//layout3col.AddColumn()
+
+	reply.RegisterLayout(layout1col)
+
+	// 标题头
+	header := scopes.NewPreviewWidget("header", "header")
+	header.AddAttributeValue("title", "<b>"+fmt.Sprint(cinema["name"])+"</b>")
+	header.AddAttributeValue("subtitle", cinema["address"])
+
+	// 地点坐标参数
+	location := cinema["location"].(map[string]interface{})
+	lng := location["lng"].(float64)
+	lat := location["lat"].(float64)
+	paramLoc := geoConv(lng, lat, true)
+	param := map[string]interface{}{
+		"center":       paramLoc,
+		"markers":      paramLoc,
+		"markerStyles": "l,o,0xff0000",
+		"zoom":         "17",
+	}
+	baiduLngLat := strings.Split(paramLoc, ",")
+	baiduMap := fmt.Sprintf("http://api.map.baidu.com/marker?location=%s,%s&title=影院地址&content=%s&output=html&src=Movie-Scope", baiduLngLat[1], baiduLngLat[0], cinema["name"])
+
+	// 静态地图
+	staticImgUrl := APIStaticImage(param)
+	param = map[string]interface{}{
+		"ak":       APP_KEY,
+		"location": paramLoc,
+		"width":    "1000",
+		"fov":      "270",
+	}
+	// 全景地图
+	panoramaImgUrl := APIPanoramaImage(param)
+
+	// 地图图片
+	mapImage := scopes.NewPreviewWidget("mapImage", "gallery")
+	mapImage.AddAttributeValue("fallback", m.base.ScopeDirectory()+"/local.png")
+	mapImage.AddAttributeValue("sources", []string{staticImgUrl, panoramaImgUrl})
+	mapImage.AddAttributeValue("zoomable", true)
+
+	info := scopes.NewPreviewWidget("info", "text")
+	info.AddAttributeValue("title", "<b>信息</b>")
+	info.AddAttributeValue(
+		"text",
+		"<b>电话: </b>"+fmt.Sprint(cinema["telephone"])+"<br/>"+
+			"<b>距离: </b>"+fmt.Sprint(cinema["distance"]))
+
+	// 按钮
+	actions := scopes.NewPreviewWidget("actions", "actions")
+	tuple1 := make(map[string]interface{})
+	tuple1["id"] = "tel"
+	tuple1["label"] = "电话咨询"
+	tuple1["uri"] = "tel:///" + fmt.Sprint(cinema["telephone"])
+	tuple2 := make(map[string]interface{})
+	tuple2["id"] = "map"
+	tuple2["label"] = "地图"
+	tuple2["uri"] = baiduMap
+	actions.AddAttributeValue("actions", []interface{}{tuple1, tuple2})
+
+	// 上映的影片图片
+	title := scopes.NewPreviewWidget("online", "text")
+	title.AddAttributeValue("title", "<b>上映的影片</b>")
+	movies := scopes.NewPreviewWidget("gallery", "gallery")
+	array := []string{}
+
+	// 上映影片信息、场次
+	onMovies := scopes.NewPreviewWidget("onMovies", "expandable")
+	onMovies.AddAttributeValue("title", "<b>影片信息及影院场次</b>")
+	onMovies.AddAttributeValue("collapsed-widgets", 2)
+
+	// 是否有电影数据
+	moviesData, ok := cinema["movies"].([]interface{})
+	if ok {
+		// 电影图片
+		for _, i := range moviesData {
+			i := i.(map[string]interface{})
+			array = append(array, fmt.Sprint(i["movie_picture"]))
+		}
+		movies.AddAttributeValue("sources", array)
+
+		// 信息场次
+		for i, m := range moviesData {
+			m := m.(map[string]interface{})
+			mInfo := scopes.NewPreviewWidget("movie"+strconv.Itoa(i), "text")
+			mInfo.AddAttributeValue("title", m["movie_name"])
+
+			// 是否有场次信息
+			tableData, ok := m["time_table"].([]interface{})
+			if ok {
+				table := "<b>评分:</b> " + fmt.Sprint(m["movie_score"]) + "<br/>" +
+					"<b>主演:</b> " + fmt.Sprint(m["movie_starring"]) + "<br/>" +
+					"<b>介绍:</b> " + fmt.Sprint(m["movie_description"]) + "<br/>" +
+					"<b>场次:</b> "
+				for _, t := range tableData {
+					t := t.(map[string]interface{})
+					time := fmt.Sprint(t["time"])
+					date := fmt.Sprint(t["date"])
+					lan := fmt.Sprint(t["lan"])
+					_type := fmt.Sprint(t["type"])
+					price := fmt.Sprint(t["price"])
+					if lan == "" && _type == "" {
+						lan = "暂无数据"
+					}
+					if price == "" {
+						price = "暂无数据"
+					}
+					table += "<br/> 时间: " + date + " " + time + "<br/>" +
+						" 类型: " + lan + " " + _type + "<br/>" +
+						" 票价: " + price + "<br/>"
+				}
+				mInfo.AddAttributeValue("text", table)
+			}
+
+			onMovies.AddWidget(mInfo)
+
+			/************************************
+			 * 这里可能是一个BUG, tt 内容不显示 *
+			 ***********************************/
+			// tt := scopes.NewPreviewWidget("tt", "expandable")
+			// tt.AddAttributeValue("title", "uuuuu")
+			// tt.AddAttributeValue("collapsed-widgets", 2)
+			// xx := scopes.NewPreviewWidget("xx", "text")
+			// xx.AddAttributeValue("title", "xxxx")
+			// xx.AddAttributeValue("text", "XXXXX")
+			// tt.AddWidget(xx)
+			// onMovies.AddWidget(tt)
+		}
+	}
+
+	// 影院评论
+	reviews := scopes.NewPreviewWidget("reviews", "expandable")
+	reviews.AddAttributeValue("title", "<b>影院评论</b>")
+	reviews.AddAttributeValue("collapsed-widgets", 2)
+
+	// 是否有评论数据
+	reviewsData, ok := cinema["review"].([]interface{})
+	if ok {
+		for i, r := range reviewsData {
+			r := r.(map[string]interface{})
+
+			rInfo := scopes.NewPreviewWidget("review"+strconv.Itoa(i), "text")
+			rInfo.AddAttributeValue("title", r["date"])
+			rInfo.AddAttributeValue("text", r["content"])
+			reviews.AddWidget(rInfo)
+		}
+	}
+
+	reply.PushWidgets(
+		mapImage,
+		header,
+		info,
+		title,
+		movies,
+		actions,
+		onMovies,
+		reviews,
+	)
 }
 
 func geoConv(lng, lat float64, t bool) string {
